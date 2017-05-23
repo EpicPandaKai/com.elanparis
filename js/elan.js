6f05a7e58195587385af250f74437f7d68bdb2ce
ShiroSensei <hescobar.dev@gmail.com>
2017-05-23 21:49:27 +0200

    header-xs

--- js/elan.js
+++ js/elan.js
@@ -32,28 +32,32 @@ var loadContact = function() {
 }
 
 /* Navigation */
-$(document).on('click', '.nav-justified > li > a#nav-home', function(e) {
+$(document).on('click', 'a.elan-nav-home', function(e) {
+    $('#elan-navbar-collapse').collapse("hide");
     var $this = $(this);
     $this.parent().siblings().removeClass('active').end().addClass('active');
     e.preventDefault();
     loadHome();
 });
 
-$(document).on('click', '.nav-justified > li > a#nav-about', function(e) {
+$(document).on('click', 'a.elan-nav-about', function(e) {
     var $this = $(this);
     $this.parent().siblings().removeClass('active').end().addClass('active');
     e.preventDefault();
     loadAbout();
+    $('#elan-navbar-collapse').collapse("hide");
 });
 
-$(document).on('click', '.nav-justified > li > a#nav-courses', function(e) {
+$(document).on('click', 'a.elan-nav-courses', function(e) {
+    $('#elan-navbar-collapse').collapse("hide");
     var $this = $(this);
     $this.parent().siblings().removeClass('active').end().addClass('active');
     e.preventDefault();
     loadCourses();
 });
 
-$(document).on('click', '.nav-justified > li > a#nav-contact', function(e) {
+$(document).on('click', 'a.elan-nav-contact', function(e) {
+    $('#elan-navbar-collapse').collapse("hide");
     var $this = $(this);
     $this.parent().siblings().removeClass('active').end().addClass('active');
     e.preventDefault();
@@ -62,18 +66,19 @@ $(document).on('click', '.nav-justified > li > a#nav-contact', function(e) {
 
 /* Email form */
 
-   var form = document.getElementsByTagName('form')[0];
-   form.addEventListener('submit',contact,false);
-   function contact(e) {
-      // Prevent Default Form Submission
-      e.preventDefault();
+    var form = document.getElementsByTagName('form')[0];
+    if (form != undefined)
+    form.addEventListener('submit',contact,false);
+    function contact(e) {
+        // Prevent Default Form Submission
+        e.preventDefault();
 
-      var target = e.target || e.srcElement;
-      var i = 0;
-      var message = '';
+        var target = e.target || e.srcElement;
+        var i = 0;
+        var message = '';
 
-      // Loop Through All Input Fields
-      for(i = 0; i < target.length; ++i) {
+        // Loop Through All Input Fields
+        for(i = 0; i < target.length; ++i) {
          // Check to make sure it's a value. Don't need to include Buttons
          if(target[i].type != 'text' && target[i].type != 'textarea') {
                 // Skip to next input since this one doesn't match our rules
@@ -82,10 +87,10 @@ $(document).on('click', '.nav-justified > li > a#nav-contact', function(e) {
 
          // Add Input Name and value followed by a line break
          message += target[i].name + ': ' + target[i].value + "\r\n";
-      }
-      // Modify the hidden body input field that is required for the mailto: scheme
-      target.elements["body"].value = message;
+        }
+        // Modify the hidden body input field that is required for the mailto: scheme
+        target.elements["body"].value = message;
 
-      // Submit the form since we previously stopped it. May cause recursive loop in some browsers? Should research this.
-      this.submit();
-   }
+        // Submit the form since we previously stopped it. May cause recursive loop in some browsers? Should research this.
+        this.submit();
+    }
